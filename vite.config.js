0f304b891f53abaf325c13a4efd921067be517e0
LightSSSDark <songarinpheinao@gmail.com>
2023-03-23 19:26:26 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/song",
+  base: "/song/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
-    assetsInclude: ['CNAME'],
   },
 })
